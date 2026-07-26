daf5e6e1cfcaaedecdb396283e30ccbb9d9953e8
pranavparikh <pranav.r.parikh@gmail.com>
2026-07-26 13:45:43 -0700

    Upgrade node version

--- test/basic.test.js
+++ test/basic.test.js
@@ -5,7 +5,7 @@ const fs = require('node:fs')
 
 test('package.json exists and declares supported Node engine', () => {
   const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package.json'), 'utf8'))
-  assert.equal(pkg.engines.node, '>=20')
+  assert.equal(pkg.engines.node, '>=26.5.0')
 })
 
 test('main entrypoint exists and is executable', () => {
