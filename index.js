92068072a9e8383be56d8c0c63afbc75f65a0796
pranavparikh <pranav.r.parikh@gmail.com>
2026-07-26 13:33:31 -0700

    Updates

--- index.js
+++ index.js
@@ -1,49 +1,65 @@
 #!/usr/bin/env node
 'use strict'
-const Program = require('commander')
+const { Command } = require('commander')
 const Debug = require('debug')('Index')
 
 const clone = require('./lib/clone')
-
 const getJsFiles = require('./lib/getJsFiles')
 const computeComplexity = require('./lib/computeComplexity')
 const generateReport = require('./lib/generateReport')
 const util = require('./lib/util')
 const version = require('./package.json').version
 
-Program
-    .version(version)
-    .usage('-g <giturl> -c <gitcheckout>')
-    .option('-g, --gitUrl [url]', 'Git Url of the project you want complexity report for')
-    .option('-c, --gitCheckout [branch/tag/checkout]', 'Branch / tag / checkout of the git project')
-    .parse(process.argv)
+const program = new Command()
 
-if (!Program.gitUrl) {
-  console.log('Missing git Url. Git url is mandatory')
-  process.exit(1)
-}
+program
+  .version(version)
+  .usage('-g <giturl> -c <gitcheckout>')
+  .option('-g, --gitUrl [url]', 'Git Url of the project you want complexity report for')
+  .option('-c, --gitCheckout [branch/tag/checkout]', 'Branch / tag / checkout of the git project')
 
-const gitUrl = Program.gitUrl
-const gitCheckout = Program.gitCheckout || 'master'
-const clonedRepoPath = util.getClonedRepoPath(gitUrl, gitCheckout)
-const reportsPath = util.getReportsPath(gitUrl, gitCheckout)
-const cloneOptions = {
-  checkout: gitCheckout
-}
-clone(gitUrl, clonedRepoPath, cloneOptions).then(function () {
-  Debug('Cloned Repo Path:' + clonedRepoPath)
-  return getJsFiles(clonedRepoPath)
-}).then(function (jsfiles) {
-  Debug('Computing complexity')
-  return computeComplexity(jsfiles, reportsPath)
-}).then(function (path) {
-  Debug('Generating reports')
-  generateReport(path, reportsPath, gitUrl)
-}).catch(function (err) {
-  if (err) {
-    console.log(err)
-  }
-}).finally(function () {
-  Debug('Cleaning up repos')
-  util.cleanupClonedRepos()
-})
+program.parseAsync(process.argv)
+  .then(function () {
+    const options = program.opts()
+
+    if (!options.gitUrl) {
+      console.log('Missing git Url. Git url is mandatory')
+      process.exit(1)
+    }
+
+    const gitUrl = options.gitUrl
+    const gitCheckout = options.gitCheckout || 'main'
+    const clonedRepoPath = util.getClonedRepoPath(gitUrl, gitCheckout)
+    const reportsPath = util.getReportsPath(gitUrl, gitCheckout)
+    const cloneOptions = {
+      checkout: gitCheckout
+    }
+
+    return clone(gitUrl, clonedRepoPath, cloneOptions)
+      .then(function () {
+        Debug('Cloned Repo Path:' + clonedRepoPath)
+        return getJsFiles(clonedRepoPath)
+      })
+      .then(function (jsfiles) {
+        Debug('Computing complexity')
+        return computeComplexity(jsfiles, reportsPath)
+      })
+      .then(function (path) {
+        Debug('Generating reports')
+        generateReport(path, reportsPath, gitUrl)
+      })
+      .catch(function (err) {
+        if (err) {
+          console.log(err)
+        }
+      })
+      .finally(function () {
+        Debug('Cleaning up repos')
+        util.cleanupClonedRepos()
+      })
+  })
+  .catch(function (err) {
+    if (err) {
+      console.log(err)
+    }
+  })
---
+++ test/basic.test.js
@@ -0,0 +1,14 @@
+const test = require('node:test')
+const assert = require('node:assert/strict')
+const path = require('node:path')
+const fs = require('node:fs')
+
+test('package.json exists and declares supported Node engine', () => {
+  const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package.json'), 'utf8'))
+  assert.equal(pkg.engines.node, '>=20')
+})
+
+test('main entrypoint exists and is executable', () => {
+  const entry = path.join(__dirname, '..', 'index.js')
+  assert.equal(fs.existsSync(entry), true)
+})
